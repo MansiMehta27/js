c8e00db9e87971f99a33cf6136633e70a38c2cf7
MehtaMansi27 <mehtamansi740@gmail.com>
2022-04-27 18:28:45 +0530

    welcome to reacte

--- src/App.js
+++ src/App.js
@@ -2,6 +2,37 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+      // let a = 10;
+      // console.log(a);
+      {
+          // let a;
+          // a=20;
+          // console.log(a);
+      }
+      {
+          // const b=40;
+          // console.log(b);
+
+      }
+      // {
+      //   const b;
+      //   b=23;
+      //   console.log(b);
+      // }   //error
+
+      {
+        // let a=45;
+        // a=a+2;
+        // console.log(a);
+      }
+
+      let arr =[10,20,30,40,50];
+      arr.map((a,i) => console.log(a));
+
+      let intro=()=>{
+        console.log("mansi mehta");
+      }
+      intro();
   return (
     <div className="App">
       <header className="App-header">
