fb4271a634592a43b8fa366b7b1f9596fed78e06
MehtaMansi27 <mehtamansi740@gmail.com>
2022-04-28 19:07:51 +0530

    28-4-22

--- src/App.js
+++ src/App.js
@@ -26,14 +26,86 @@ function App() {
         // console.log(a);
       }
 
-      let arr =[10,20,30,40,50];
-      arr.map((a,i) => console.log(a));
+      // let arr =[10,20,30,40,50];
+      // arr.map((a,i) => console.log(a));
 
-      let intro=()=>{
-        console.log("mansi mehta");
-      }
-       intro()
-       
+      // let intro=()=>{
+      //   console.log("mansi mehta");
+      // }
+      //  intro()
+
+
+        //array//
+
+
+      //  let arr = [10,"mansi",20,30,40,50];
+      //  console.log(arr[4]);
+
+       //object//
+
+
+      //  let obj ={
+      //    id:10,
+      //    age:20,
+      //    per:30
+
+      //  }
+      //  console.log(obj.age);
+
+      //push//
+
+      //  let arr = [10,"mansi",20,30,40,50];
+      //  arr.push(99);
+
+      //  console.log(arr);
+
+      //unshift//
+
+      //  let arr = [10,"mansi",20,30,40,50];
+      //  arr.unshift(23);
+      //  console.log(arr);
+
+      // pop //
+
+      // let arr =[10,20,30,40,50];
+      // arr.pop();
+      // console.log(arr);
+
+      //shift//
+
+      // let arr =[10,20,30,40,50];
+      // arr.shift();
+      // console.log(arr);
+
+    //splice//
+
+    //    let arr =[10,20,30,40,50];
+    //   arr.splice(3,0,99);
+    //  console.log(arr);
+
+      //tostring//
+
+      //  let arr = [10,20,30,40,50];
+      //  console.log(arr.toString());
+
+    //some//
+    // let arr = [10,20,30,40,50];
+    // let res=arr.some((a)=>a>100);
+    // console.log(res);
+
+       //find//
+
+      //  let arr = [10,20,30,40,50];
+      //  let res=arr.find((a)=>a>10);
+      //  console.log(res);
+
+      //isarray//
+      let arr=[10,20,30,40,50];
+      let res = Array.isArray(arr);
+      console.log(res);
+
+
+
   return (
     <div className="App">
       <header className="App-header">
