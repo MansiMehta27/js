448a79a64e5e36a257f33871c7aed116d19aab88
MehtaMansi27 <mehtamansi740@gmail.com>
2022-05-04 19:07:46 +0530

    Country

---
+++ src/Container/Country/Country.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+
+export default class Country extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            Country: 'India'
+        };
+    }
+    ChangeCountry = () => {
+        this.setState({
+            Country: 'UK'
+        });
+    }
+    render() {
+        return (
+            <>
+                <div>Country:{this.state.Country}</div>
+                <button onClick={() => this.ChangeCountry()}>Change</button>
+            </>
+        )
+    }
+}
+
+
