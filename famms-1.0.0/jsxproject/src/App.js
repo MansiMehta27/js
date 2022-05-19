08203c01960cd14754ea01b64b540e28b2881811
MehtaMansi27 <mehtamansi740@gmail.com>
2022-05-19 19:36:03 +0530

    finish

--- famms-1.0.0/jsxproject/src/App.js
+++ famms-1.0.0/jsxproject/src/App.js
@@ -1,11 +1,24 @@
+import { Route } from "react-router-dom";
+import { Switch } from "react-router-dom/cjs/react-router-dom.min";
 import Footer from "./Componet/Footer/Footer";
 import Header from "./Componet/Header/Header";
+import Blog from "./Container/Blog/Blog";
+import Contect from "./Container/Contect/Contect";
+// import About from "./Container/About/About";
+import Home from "./Container/Home/Home";
+import Product from "./Container/products/Product";
 
 function App() {
   return (
     <>
               <Header/>
-                                 
+                <Switch>
+                              <Route exact path={"/"} component={Home} />
+                              {/* <Route exact path={"/about"} component={About}/> */}
+                              <Route exact path={"/product"} component={Product}/>
+                              <Route exact path={"/blog"}component={Blog}/>
+                              <Route exact path={"/contect"}component={Contect}/>
+                </Switch>   
               <Footer/>
 
     </>
