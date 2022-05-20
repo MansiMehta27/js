e881cc50314541c4b56fb692a72af69be0634ab0
MehtaMansi27 <mehtamansi740@gmail.com>
2022-05-20 19:23:33 +0530

    hello

---
+++ Klean Free Website Template - Free-CSS.com/finalproject/src/App.js
@@ -0,0 +1,28 @@
+import { Route } from "react-router-dom";
+import { Switch } from "react-router-dom";
+import Footer from "./Componet/Footer/Footer";
+import Header from "./Componet/Header/Header";
+import About from "./Container/About/About";
+import Contect from "./Container/Contect/Contect";
+import Home from "./Container/Home/Home";
+import Project from "./Container/Project/Project";
+import Service from "./Container/Service/Service";
+
+
+function App() {
+  return (
+          <>
+                  <Header/>
+                                <Switch>
+                                        <Route exact path={"/"} component={Home}/>
+                                        <Route exact path={"/about"} component={About}/>
+                                        <Route exact path={"/service"}component={Service}/>
+                                        <Route exact path={"/project"}component={Project}/>
+                                        <Route exact path={"/contect"}component={Contect}/>
+                                </Switch>
+                  <Footer/>
+          </>
+  );
+}
+
+export default App;
