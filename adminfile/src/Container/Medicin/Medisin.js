1f28e35a452206ef4305ab0d552fe2ea3454656b
MehtaMansi27 <mehtamansi740@gmail.com>
2022-06-03 19:28:42 +0530

    hye

--- adminfile/src/Container/Medicin/Medisin.js
+++ adminfile/src/Container/Medicin/Medisin.js
@@ -7,101 +7,168 @@ import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 import { useState } from 'react';
+import { DataGrid } from '@mui/x-data-grid';
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { useEffect } from 'react';
 
 function Medisin(props) {
-    const [open, setOpen] = useState(false);
-    const [name,setname] = useState('');
-    const [price,setprice] = useState('');
-    const [quantity,setquantity] = useState('');
-    const [expiry,setexpiry] = useState('');
+  const [open, setOpen] = useState(false);
+  const [name, setname] = useState('');
+  const [price, setprice] = useState('');
+  const [quantity, setquantity] = useState('');
+  const [expiry, setexpiry] = useState('');
+  const [data, setdata] = useState([]);
 
-    const handleClickOpen = () => {
-        setOpen(true);
-    };
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
 
-    const handleClose = () => {
-        setOpen(false);
-    };
-    const handlesubmit =()=>{
-            let data={
-                id: Math.floor(Math.random() * 1000),
-                name,
-                price,
-                quantity,
-                expiry
-            };
-            localStorage.setItem('medisin', JSON.stringify(data));
-            handleClose();
-            setname('');
-            setprice('');
-            setquantity('');
-            setexpiry('');
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const getdata = () => {
 
-            console.log(data);
+    let localdata = JSON.parse(localStorage.getItem('medisin'));
+    if (localdata !== null) {
+      setdata(localdata);
     }
-    return (
-        <div>
-        <Button variant="outlined" onClick={handleClickOpen}>
-        add medisin
-        </Button>
-        <Dialog open={open} onClose={handleClose}>
-          <DialogTitle>medisin data</DialogTitle>
-          <DialogContent>
-           
-            <TextField
-              autoFocus
-              margin="dense"
-              id="name"
-              name='name'
-              label="Medisin Name"
-              type="name"
-              fullWidth
-              variant="standard"
-              onChange={(e) => setname (e.target.value)}
-            />
-             <TextField
-              autoFocus
-              margin="dense"
-              id="name"
-              name='price'
-              label="Medisin Price"
-              type="price"
-              fullWidth
-              variant="standard"
-              onChange={(e) => setprice (e.target.value)}
-            />
-             <TextField
-              autoFocus
-              margin="dense"
-              id="name"
-              name='quantity'
-              label="Medisin Quantity"
-              type="quantity"
-              fullWidth
-              variant="standard"
-              onChange={(e) => setquantity (e.target.value)}
-            />
-             <TextField
-              autoFocus
-              margin="dense"
-              id="name"
-              name='expiry'
-              label="Medisin Expiry"
-              type="expiry"
-              fullWidth
-              variant="standard"
-              onChange={(e) => setexpiry (e.target.value)}
-            />
-            
+  }
+  const handleDelet =(params)=>{
+      let localdata1=JSON.parse(localStorage.getItem("medisin"));
+     let appdata = localdata1.filter((l,i)=>l.id!==params.id);
+     localStorage.setItem("medisin",JSON.stringify(appdata));
 
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={handleClose}>Cancel</Button>
-            <Button onClick={handlesubmit}>submit</Button>
-          </DialogActions>
-        </Dialog>
+     getdata();
+  }
+  useEffect(
+    () => {
+      getdata();
+    },
+  [])
+  const handlesubmit = () => {
+    let data = {
+      id: Math.floor(Math.random() * 1000),
+      name,
+      price,
+      quantity,
+      expiry
+    };
+
+    let localdata = JSON.parse(localStorage.getItem('medisin'));
+    if (localdata === null) {
+      localStorage.setItem('medisin', JSON.stringify([data]));
+    }
+    else {
+      localdata.push(data)
+      localStorage.setItem('medisin', JSON.stringify(localdata));
+    }
+
+    handleClose();
+    setname('');
+    setprice('');
+    setquantity('');
+    setexpiry('');
+    getdata();
+    
+    // console.log(data);
+  }
+
+  const columns = [
+    { field: 'id', headerName: 'ID', width: 70 },
+    { field: 'name', headerName: 'Name', width: 130 },
+    { field: 'price', headerName: 'Price', width: 130 },
+    { field: 'quantity', headerName: 'Quantity', width: 130 },
+    { field: 'expiry', headerName: 'Expiry', width: 130 },
+    {
+      field: 'action',
+      headerName: 'Action',
+      width: 130,
+      renderCell: (params) => {
+        return (
+          <IconButton aria-label="delete" onClick={()=>handleDelet(params)}>
+            <DeleteIcon />
+          </IconButton>
+        )
+      }
+    },
+
+
+
+  ]
+  return (
+    <div>
+      <Button variant="outlined" onClick={handleClickOpen}>
+        add medisin
+      </Button>
+
+      <div style={{ height: 400, width: '100%' }}>
+        <DataGrid
+          rows={data}
+          columns={columns}
+          pageSize={5}
+          rowsPerPageOptions={[5]}
+          checkboxSelection
+        />
       </div>
-    );
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>medisin data</DialogTitle>
+        <DialogContent>
+
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            name='name'
+            label="Medisin Name"
+            type="name"
+            fullWidth
+            variant="standard"
+            onChange={(e) => setname(e.target.value)}
+          />
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            name='price'
+            label="Medisin Price"
+            type="price"
+            fullWidth
+            variant="standard"
+            onChange={(e) => setprice(e.target.value)}
+          />
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            name='quantity'
+            label="Medisin Quantity"
+            type="quantity"
+            fullWidth
+            variant="standard"
+            onChange={(e) => setquantity(e.target.value)}
+          />
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            name='expiry'
+            label="Medisin Expiry"
+            type="expiry"
+            fullWidth
+            variant="standard"
+            onChange={(e) => setexpiry(e.target.value)}
+          />
+
+
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={handlesubmit}>submit</Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
 }
 
 export default Medisin;
